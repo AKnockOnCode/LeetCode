b46567d6dcb605219c78cc029a75fa61000fb675
AKnockOnCode <akshitkoul2005@gmail.com>
2026-09-10 22:45:40 +0530

    Time: 2 ms (3.03%), Space: 8.7 MB (7.07%) - LeetHub

---
+++ 3908-valid-digit-number/3908-valid-digit-number.c
@@ -0,0 +1,19 @@
+bool validDigit(int n, int x) {
+    int arr[100000];
+    int i=0;
+    int a = n;
+    int count = 0;
+    while (a){
+        arr[i++]=a%10;
+        a/=10;
+        count++;
+    }
+    int check = 0;
+    for (i=0;i<count;i++){
+        if (arr[i]==x){
+            check = 1;
+        }
+    }
+    if (n!=0 && arr[count-1]!=x && check ==1)return true;
+    return false;
+}
