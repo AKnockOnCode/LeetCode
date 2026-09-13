a08b2d9430379fae330bee40ee7fe627036ce081
AKnockOnCode <akshitkoul2005@gmail.com>
2026-09-13 13:03:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (66.98%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.c
+++ 3945-digit-frequency-score/3945-digit-frequency-score.c
@@ -1,8 +1,4 @@
 int digitFrequencyScore(int n) {
-    int sum = 0;
-    while (n){
-        sum += n%10;
-        n /= 10;
-    }
-    return sum;
+    if (n==0) return 0;
+    return n%10 + digitFrequencyScore(n/10);
 }
